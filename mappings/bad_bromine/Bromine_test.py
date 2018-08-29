0485a31a3cfde64e607dc7d466091fbf65c96732
Lester Hedges <lester.hedges@gmail.com>
2018-08-29 12:32:28 +0100

    Updated Bromine test. Works if Bromine coordinates shifted slightly.

--- mappings/bad_bromine/Bromine_test.py
+++ mappings/bad_bromine/Bromine_test.py
@@ -1,18 +1,15 @@
 import BioSimSpace as BSS
 
-
 if __name__ == '__main__':
 
-    # problematic file
-    filename = 'FXR_84_BM2.mol2'
+    # Load the molecule.
+    mol0 = BSS.IO.readMolecules("FXR_84_BM2.mol2").getMolecules()[0]
+    mol1 = BSS.IO.readMolecules("FXR_84_BM2_shifted.mol2").getMolecules()[0]
 
-    # loading the molecule
-    mol0 = BSS.IO.readMolecules(filename).getMolecules()[0]
+    # Get the best mapping.
+    mapping0 = BSS.Align.matchAtoms(mol0, mol0)
+    mapping1 = BSS.Align.matchAtoms(mol1, mol1)
 
-    mapping = BSS.Align.matchAtoms(mol0, mol0)
-
-    for idx0, idx1 in mapping.items():
-        atom0 = mol0._getSireMolecule().atom(idx0)
-        atom1 = mol0._getSireMolecule().atom(idx1)
-        map_string = "%s --> %s" % (atom0, atom1)
-        print (map_string)
+    # Make sure all of the atoms have been mapped.
+    print("Unshifted ==> Mapped %d out of %d atoms." % (len(mapping0), mol0.nAtoms()))
+    print("Shifted   ==> Mapped %d out of %d atoms." % (len(mapping1), mol1.nAtoms()))
